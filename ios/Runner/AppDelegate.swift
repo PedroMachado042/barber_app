1be9db0418e083f2c6148f4324614d812bd4414b
Pedro Machado <machado.trabalhos@gmail.com>
2025-07-22 20:44:06 -0300

    ios runner changes
    app delegate, googleservice, info.plist

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,14 @@
 import UIKit
 import Flutter
+import Firebase
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  override func application(
-    _ app: UIApplication,
-    open url: URL,
-    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-    return super.application(app, open: url, options: options)
   }
 }
